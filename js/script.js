f458e26752347c83868c8b4d98afab5a18b54d3e
amarkoju1 <165230298+amarkoju1@users.noreply.github.com>
2024-03-28 12:08:45 +0900

    script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 window.onload = function() {
-    // Get all elements with class "yt-link"
-    var ytLinks = document.querySelectorAll('.yt-link');
+    // Get the element with class "yt-link"
+    var ytLink = document.querySelector('.yt-link');
 
-    // Loop through each element and embed the YouTube video
-    ytLinks.forEach(function(ytLink) {
-        // Get the YouTube link text content
-        var youtubeLink = ytLink.textContent.trim();
+    // Get the YouTube link text content
+    var youtubeLink = ytLink.textContent.trim();
 
-        // Extract video ID from the YouTube link
-        var videoID = youtubeLink.split('v=')[1];
-        var ampersandPosition = videoID.indexOf('&');
-        if (ampersandPosition !== -1) {
-            videoID = videoID.substring(0, ampersandPosition);
-        }
+    // Extract video ID from the YouTube link
+    var videoID = youtubeLink.split('v=')[1];
+    var ampersandPosition = videoID.indexOf('&');
+    if (ampersandPosition !== -1) {
+        videoID = videoID.substring(0, ampersandPosition);
+    }
 
-        // Construct the iframe HTML
-        var iframeHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoID + '?rel=0" frameborder="0" allowfullscreen></iframe>';
+    // Construct the iframe HTML
+    var iframeHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoID + '" frameborder="0" allowfullscreen></iframe>';
 
-        // Insert the iframe HTML into the parent of the element with class "yt-link"
-        var ytVideoParent = ytLink.parentElement;
-        ytVideoParent.innerHTML = iframeHTML;
-    });
+    // Insert the iframe HTML into the div with class "yt-video"
+    var ytVideoDiv = document.querySelector('.yt-video');
+    ytVideoDiv.innerHTML = iframeHTML;
 }
